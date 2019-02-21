26543e66081875eded77acefda9b88bf62650f60
jessix00 <jesscasillas24@gmail.com>
2019-02-21 13:49:25 -0700

    New function adds height to hero section when hamburger menu is clicked.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //Toggles our hamburger menu on/off 
 function toggleMenu() {
     var hiddenMenu = document.getElementById('burger-nav');
-    var addHeight = document.getElementsByClassName('hero-section');
     hiddenMenu.classList.toggle('hidden');
 }
-
+//adds height to the hero-section below the navigation
+function addHeight() {
+    var addHeight = document.getElementById('hero-section');
+    addHeight.classList.toggle('height');
+}
 
 //event listeners
 //listens for the hamburger icon to be cliecked and calls toggleMenu function
 document.getElementById('burger').addEventListener('click', toggleMenu);
+//listens for the hamburger icon to be cliecked and calls addHeight function
+document.getElementById('burger').addEventListener('click', addHeight);
